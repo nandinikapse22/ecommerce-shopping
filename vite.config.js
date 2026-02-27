54da01cda1194f227f566227f0e83fb68840b99a
Nandini Kapse <nandinikapse22@gmail.com>
2026-02-27 15:52:18 +0530

    Set base path for ecommerce shopping

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/ecommerce-shopping/",   // 👈 ADD THIS LINE
   plugins: [react()],
 })
